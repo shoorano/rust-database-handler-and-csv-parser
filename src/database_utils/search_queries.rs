22c81523ad1da27a9b8ac2e5ef0bdff0194601d2
Shoorano <danwilloughby2@gmail.com>
2022-03-04 20:03:34 +0000

    finished first iter of fetching a single row, moving onto fetching mutliple rows

--- src/database_utils/search_queries.rs
+++ src/database_utils/search_queries.rs
@@ -1,8 +1,6 @@
-use super::database::*;
-use super::utils::*;
 use mysql::*;
-use mysql::prelude::*;
-use chrono::naive::{NaiveDate, NaiveTime, NaiveDateTime};
+use super::database::*;
+use chrono::naive::NaiveDateTime;
 
 #[derive(Debug, PartialEq, PartialOrd)]
 pub struct SearchQuery {
@@ -28,6 +26,7 @@ pub struct SearchQuery {
 }
 
 impl SearchQuery {
+    /// creates a SearchQuery struct
     pub fn init(
         id: String,
         created_at: Option<NaiveDateTime>,
@@ -71,6 +70,8 @@ impl SearchQuery {
             graph_order
         }
     }
+    /// receives a mysqsl::Value::Row struct
+    /// returns a SearchQuery struct from row data
     pub fn from_row(row: &mut Row) -> Self {
         Self::init(
             row.take("id").unwrap(),
@@ -94,6 +95,33 @@ impl SearchQuery {
             row.take("graph_order"),
         )
     }
+    /// create the search_query table
+    pub fn create_table(conn: &mut PooledConn) {
+        let create_table_string = "
+            create table search_queries (
+            id char(36) not null primary key,
+            created_at timestamp,
+            updated_at timestamp,
+            account_id char(36) not null,
+            search_query_id char(36) not null,
+            date_range char(100),
+            impressions int,
+            clicks int,
+            conversions int,
+            average_position decimal(11,1),
+            cost decimal(11,2),
+            average_cpc decimal(11,2),
+            conversion_value decimal(11,2),
+            cpa decimal(11,2),
+            roas decimal(11,2),
+            conversion_rate decimal(11,2),
+            ctr decimal(11,2),
+            show_on_graph varchar(191),
+            graph_order varchar(191)
+        );
+        ".to_owned();
+        create_table(conn, create_table_string)
+            .expect("could not create search_query table");
+    }
 }
-
 
--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,16 @@
 pub mod database_utils {
     pub mod search_queries;
     pub mod utils;
-    pub mod database; // because it's not `pub` it won't be visible outside of `ui`
+    pub mod database; 
 }
 use database_utils::search_queries::*;
-use database_utils::utils::*;
 use database_utils::database::*;
 
 fn main() {
     let mut conn = get_conn().unwrap();
-    let create_table_string = "
-        create table search_queries (
-        id char(36) not null primary key,
-        created_at timestamp,
-        updated_at timestamp,
-        account_id char(36) not null,
-        search_query_id char(36) not null,
-        date_range char(100),
-        impressions int,
-        clicks int,
-        conversions int,
-        average_position decimal(11,1),
-        cost decimal(11,2),
-        average_cpc decimal(11,2),
-        conversion_value decimal(11,2),
-        cpa decimal(11,2),
-        roas decimal(11,2),
-        conversion_rate decimal(11,2),
-        ctr decimal(11,2),
-        show_on_graph varchar(191),
-        graph_order varchar(191)
-    );
-    ".to_owned();
-
     let query = "select * from search_queries;".to_owned();
-    query_table_new(&mut conn, query);
+    let search_query = SearchQuery::from_row(
+        &mut query_table_new(&mut conn, query).unwrap()
+    );
+    println!("{:?}", search_query);
 }
